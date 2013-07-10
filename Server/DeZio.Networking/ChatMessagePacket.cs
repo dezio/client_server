b2626345c4239248449cd76f8f9a40cda6b3e6db
DeZio <dezio@live.de>
2013-07-10 15:45:47 +0200

    Code cleanup

--- Server/DeZio.Networking/ChatMessagePacket.cs
+++ Server/DeZio.Networking/ChatMessagePacket.cs
@@ -1,36 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// (C) 2013 - Dennis Ziolkowski
+// Server : ChatMessagePacket.cs
+
+#region Usings
+
+using System;
 using DeZio.Networking.Packet;
+
+#endregion
 
 namespace DeZio.Networking {
     public class ChatMessagePacket {
+        public ChatMessagePacket() {}
+        public String Message { get; set; }
 
-        public String Message {
-            get; set; }
+        public int FromUserId { get; set; }
 
-        public int FromUserId {
-            get; set; }
-
-        public int ToUserId {
-            get;
-            set;
-        }
+        public int ToUserId { get; set; }
 
         public MessagePacket Packet {
             get {
-                return new MessagePacket() {
-                    Type = "Message",
-                    Message = string.Format("from={0}&to={1}&msg={2}",
-                    FromUserId, ToUserId, Message)
-                };
+                return new MessagePacket()
+                    {
+                        Type = "Message",
+                        Message = string.Format("from={0}&to={1}&msg={2}",
+                                                FromUserId, ToUserId, Message)
+                    };
             }
-        }
-
-        public ChatMessagePacket() {
-            
         }
     }
 }
